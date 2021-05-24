28ae4af154a764f89404582bb659fcbf58e8b6d0
Bertozzi Julien <bertozzijulien@gmail.com>
2021-05-24 16:11:01 +0200

    Update color-cycle.asm
    kick assembler update

--- color-cycle.asm
+++ color-cycle.asm
@@ -1,11 +1,14 @@
-	*=$0801
-loop:
-	lda $d012
-	cmp #$00
-	bne loop
-	inx
-	cpx	#$40
-	bne loop
-	inc $d020
-	dec $d021
-	jmp loop	
+        *= $0801 "Basic Upstart"
+        BasicUpstart(start)    // 10 sys$0810
+
+        *= $0810 "Program"
+start:
+        lda $d012
+        cmp #$00
+        bne start
+        inx
+        cpx	#$40
+        bne start
+        inc $d020
+        dec $d021
+        jmp start

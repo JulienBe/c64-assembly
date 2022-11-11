f1a445307eade9444db8c31f9bf33cb64ad45eb3
Julien Bertozzi <bertozzijulien@gmail.com>
2022-11-11 14:14:27 +0100

    hello world: v1

--- hello_world.asm
+++ hello_world.asm
@@ -1,5 +1,26 @@
-BasicUpstart2(start)                    // There is a second variation of the basic upstart macro that also takes care of setting up memory blocks:
+BasicUpstart2(start)                // There is a second variation of the basic upstart macro that also takes care of setting up memory blocks:
 
-start:  inc $d020
-        inc $d021
-        jmp start
+* = $4000
+start:
+    lda #$00
+    sta $d020                       // Border color             : 0
+    sta $d021                       // Background color         : 0
+    lda #$01
+    sta $0286                       // Cursor address           : 1
+    jsr $e544                       // Clear screen function
+    jsr draw_text
+    jmp *
+
+msg:
+    .text "              hello, world!             "
+
+draw_text:
+    ldx #$00
+
+draw_loop:
+    lda msg,x
+    sta $05e0,x
+    inx
+    cpx #$28                        // 40 characters per line
+    bne draw_loop
+    rts
---
+++ readfme.md
@@ -0,0 +1,7 @@
+# Build
+
+build and run with `./build.sh hello_world.asm`
+
+# Useful links
+
+http://sta.c64.org/cbm64mem.html

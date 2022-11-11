c8d392437a776f4ea21ff28a17bad286804d47f5
Julien Bertozzi <bertozzijulien@gmail.com>
2022-11-11 14:00:46 +0100

    toolchain: take file WITH extension as input in the build script

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Pass only the name of the file without extension.
-#
 # Build with kick assembler located in the toolchain directory.
 # Requires Java 11 or higher.
 #
 # Run the project using Vice, which is expected to be available in the path under x64sc.
 
-echo "Compiling $1"
-java -jar toolchain/KickAss.jar "$1".asm
+filename=$(basename "$1" .asm)
 
-echo "Running $1"
-x64sc "$1".prg
+echo "###################### Compiling $1 ######################"
+java -jar toolchain/KickAss.jar "$filename".asm
+
+echo "###################### Running $filename ######################"
+x64sc "$filename".prg
